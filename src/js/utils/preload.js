878257cf5340191456ab4f56b0dedcf15954f5ec
Sikriti Dakua <38714423+devloop01@users.noreply.github.com>
2020-10-04 20:47:11 +0530

    Delete preload.js

--- src/js/utils/preload.js
+++
@@ -1,7 +0,0 @@
-import imagesLoaded from "imagesloaded";
-
-export function preloadImages(selector) {
-	return new Promise((resolve, reject) => {
-		imagesLoaded(selector, { background: true }, resolve);
-	});
-}
